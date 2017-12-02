92a1519782d6ac413736062b8ee08877dfe9c538
Adam Johnson <me@adamj.eu>
2017-12-02 23:30:22 +0000

    Test with Django 2.0

--- setup.py
+++ setup.py
@@ -56,6 +56,7 @@ setup(
         'Framework :: Django :: 1.9',
         'Framework :: Django :: 1.10',
         'Framework :: Django :: 1.11',
+        'Framework :: Django :: 2.0',
         'Intended Audience :: Developers',
         'Intended Audience :: System Administrators',
         'License :: OSI Approved :: Apache Software License',
